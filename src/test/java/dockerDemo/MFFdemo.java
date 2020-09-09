7739ef4f026bd41f6b28a2376e6c8be5ff563b24
Mohammed <mirmohammed.ali@tafensw.com.au>
2020-09-09 19:21:43 +1000

    added docker and Zalenium stroy with single test case

---
+++ src/test/java/dockerDemo/MFFdemo.java
@@ -0,0 +1,30 @@
+package dockerDemo;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class MFFdemo {
+	
+	@Test
+	public void firefoxApp() throws MalformedURLException, InterruptedException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setBrowserName(BrowserType.FIREFOX);
+//		cap.setBrowserName(BrowserType.CHROME);
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4545/wd/hub"), cap); // instead of http://localhost:4545/grid/console
+		
+		driver.get("http://google.com");
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+}
